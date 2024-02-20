d89ebe8284a7fd34f87d8b4bde50a228d50ce383
francis-santos <francis.santos@ymail.com>
2024-02-20 20:56:47 -0300

    feat: Add navigation drawer in the dashboard screen

--- test/dashboard_screen_test.dart
+++ test/dashboard_screen_test.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:rota_gourmet/features/dashboard/presentation/dashboard_screen.dart';
 
-void main() {
-  group('DashboardScreen', () {
-    testWidgets('Renders correctly', (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const MaterialApp(
-        home: DashboardScreen(),
-      ));
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
 
-      // Verify that text is rendered
-      expect(find.text('Bem-vindo à página Dashboard!'), findsOneWidget);
-    });
-  });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Implemente a navegação para o item 1 aqui
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Implemente a navegação para o item 2 aqui
+              },
+            ),
+            // Adicione mais itens conforme necessário
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Bem-vindo à página Dashboard!',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
 }
