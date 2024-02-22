e970dbd5c47e69d3364ab411544a1771d6f13f1e
francis-santos <francis.santos@ymail.com>
2024-02-21 22:00:02 -0300

    feat: Add map in the dashboard

--- lib/features/dashboard/presentation/dashboard_screen.dart
+++ lib/features/dashboard/presentation/dashboard_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  DashboardScreenState createState() => DashboardScreenState();
+}
+
+class DashboardScreenState extends State<DashboardScreen> {
+  late GoogleMapController _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -9,10 +17,45 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
-      body: const Center(
-        child: Text(
-          'Bem-vindo à página Dashboard!',
-          style: TextStyle(fontSize: 24.0),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Implemente a navegação para o item 1 aqui
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Implemente a navegação para o item 2 aqui
+              },
+            ),
+            // Adicione mais itens conforme necessário
+          ],
+        ),
+      ),
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        },
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(37.42796133580664, -122.085749655962),
+          zoom: 14.0,
         ),
       ),
     );
